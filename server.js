f339f71ca90ffc7d5da4fae51c26aa058bce8dd9
Rhawan Brenner Silva Franco <rhawan-bs@detran.go.gov.br>
2019-05-06 15:44:00 -0300

    Web chat real time using NodeJS + web socket

--- server.js
+++ server.js
@@ -14,8 +14,18 @@ app.use("/", (req, res) => {
   res.render("index.html");
 });
 
-io.on('connection', socket => {
-    console.log(`Socket conectado: ${socket.id}`);
-})
+let messages = [];
+
+io.on("connection", socket => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.emit("previousMessages", messages);
+
+  socket.on("sendMessage", data => {
+    messages.push(data);
+
+    socket.broadcast.emit("receivedMessage", data);
+  });
+});
 
 server.listen(3000);
